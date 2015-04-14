04f0b4851943f829b44bc611e32a357f04b959b6
HunterIT <kenneth@hunterit.ca>
2015-04-14 14:12:44 -0400

    added Linux Script

---
+++ Linux/midnight.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#######################################
+# Name: Midnight.sh
+# Author: Kenneth & Chandan
+# Platform: Any Linux Distro
+# Tested on: Arch Linux, Fedora, Ubuntu
+# 
+# Purpose: To perfom troll applications to change settings/ open content
+#		- Open 50 Webpages of Tastyblacks.com
+#######################################
+
+echo "Hello World";
+
